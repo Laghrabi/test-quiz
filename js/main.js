b5f1a3af680ba6ea1693e25aa6ebd6d0ee367bbc
usri.rachid <usri.rachid@gmail.com>
2024-04-29 00:02:30 +0100

    modify burger navbar

--- js/main.js
+++ js/main.js
@@ -611,3 +611,12 @@ btnj.addEventListener("click", getJoke);
 getJoke();
 
 //toggle
+// JavaScript code for responsive behavior
+
+const burger = document.querySelector(".burger");
+const navLinks = document.querySelector(".nav-links");
+
+burger.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+  burger.classList.toggle("toggle");
+});
